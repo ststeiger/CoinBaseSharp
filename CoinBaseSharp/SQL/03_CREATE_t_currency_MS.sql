7ff1b9d39760111bbddc6916038368c9b3b4d814
root <quandary82@hailmail.net>
2016-05-13 08:23:46 +0200

    03_CREATE_t_currency_MS.sql:

--- CoinBaseSharp/SQL/03_CREATE_t_currency_MS.sql
+++ CoinBaseSharp/SQL/03_CREATE_t_currency_MS.sql
@@ -5,10 +5,11 @@ CREATE TABLE dbo.t_currency
 ( 
 	 ccy_uid uniqueidentifier NOT NULL 
 	,ccy_number int NULL 
-	,ccy_name nvarchar(255) NULL 
-	,ccy_abbreviation nvarchar(20) NULL 
-	,ccy_country_name nvarchar(255) NULL 
-	,ccy_minor_units nvarchar(20) NULL 
+	,ccy_name national character varying(255) NULL 
+	,ccy_abbreviation national character varying(20) NULL 
+	,ccy_country_name national character varying(255) NULL 
+	,ccy_name_normalized national character varying(255) NULL
+	,ccy_minor_units national character varying(20) NULL 
 	,CONSTRAINT PK_t_currency PRIMARY KEY (ccy_uid) 
 );
 ');
@@ -18,3 +19,17 @@ CREATE TABLE dbo.t_currency
 -- UPDATE t_currency SET ccy_minor_units = NULL WHERE ccy_minor_units = 'N.A.' 
 
 -- ALTER TABLE dbo.t_currency ALTER COLUMN ccy_minor_units int NULL
+
+CREATE TABLE IF NOT EXISTS t_currency 
+( 
+	 ccy_uid uuid NOT NULL 
+	,ccy_number int NULL 
+	,ccy_name national character varying(255) NULL 
+	,ccy_abbreviation national character varying(20) NULL 
+	,ccy_country_name national character varying(255) NULL 
+	,ccy_name_normalized national character varying(255) NULL 
+	,ccy_minor_units national character varying(20) NULL 
+	,CONSTRAINT PK_t_currency PRIMARY KEY (ccy_uid) 
+);
+
+-- ALTER TABLE t_currency ALTER COLUMN ccy_minor_units type int using (ccy_minor_units::int)  
