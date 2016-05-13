c396702df853c07d4768ab12980e2335621e5e4d
ststeiger <quandary82@hailmail.net>
2016-05-13 17:18:50 +0200

    Extended config table

---
+++ CoinBaseSharp/Exchanges/Bitfinex/Bitfinex.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace CoinBaseSharp.Exchanges.Bitfinex
+{
+
+
+    class Bitfinex
+    {
+
+        // https://api.bitfinex.com/v1/ticker/btcusd
+        // https://api.bitfinex.com/v1/balances
+        // https://api.bitfinex.com/v1/balances
+        // https://api.bitfinex.com/v1/order/status
+        // https://api.bitfinex.com/v1/positions
+
+
+        // bids/ask:
+        // https://api.bitfinex.com/v1/book/btcusd
+        // https://api.bitfinex.com/v1/book/btcusd
+
+    }
+
+
+}
--- CoinBaseSharp/SQL/01_Create_t_api_configurations_MS.sql
+++ CoinBaseSharp/SQL/01_Create_t_api_configurations_MS.sql
@@ -7,6 +7,8 @@ CREATE TABLE dbo.t_api_configurations
 	 api_uid uniqueidentifier NOT NULL 
 	,api_name national character varying(300) NULL 
 	,api_app_id national character varying(300) NULL 
+	,api_key_public national character varying(300) NULL 
+	,api_key_secret national character varying(300) NULL 
 	,api_href national character varying(4000) NULL 
 	,api_comment national character varying(4000) NULL 
 	,CONSTRAINT PK_t_api_configurations PRIMARY KEY(api_uid) 
