909b767afbff8e2612a1c246cb9cb22dcac02ebe
root <quandary82@hailmail.net>
2016-05-24 08:23:22 +0200

    * CoinBaseSharp.csproj: * btcusd.txt: * symbols.txt: * Bitfinex.cs:
    * CoinBaseSharp.userprefs: JSON

--- CoinBaseSharp/Exchanges/Bitfinex/Bitfinex.cs
+++ CoinBaseSharp/Exchanges/Bitfinex/Bitfinex.cs
@@ -10,6 +10,27 @@ namespace CoinBaseSharp.Exchanges.Bitfinex
     {
 
         // https://api.bitfinex.com/v1/ticker/btcusd
+        // http://docs.bitfinex.com/#ticker
+        public class TickerRootObject
+        {
+            public decimal mid { get; set; }
+            public decimal bid { get; set; }
+            public decimal ask { get; set; }
+            public decimal last_price { get; set; }
+            public decimal timestamp { get; set; }
+        }
+
+
+        // https://api.bitfinex.com/v1/symbols
+        // A list of symbol names. Currently “btcusd”, “ltcusd”, “ltcbtc”, “ethusd”, “ethbtc”
+        // {"foo": ["btcusd","ltcusd","ltcbtc","ethusd","ethbtc"] }
+        public class SymbolsRootObject
+        {
+            public List<string> foo { get; set; }
+        }
+
+
+
         // https://api.bitfinex.com/v1/balances
         // https://api.bitfinex.com/v1/balances
         // https://api.bitfinex.com/v1/order/status
